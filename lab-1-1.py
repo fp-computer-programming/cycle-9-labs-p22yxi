1fe086716fdb6737ff5b11c6d0c8cb8b65835e58
p22yxi <p22yxi@fairfieldprep.org>
2022-01-06 10:49:02 -0500

    lab-1-1

---
+++ lab-1-1.py
@@ -0,0 +1,12 @@
+rows = [["Mateo", "Jason", "Jordan", "Mohamed", "Michael", "Charlie", "Declan"], ["Sean", "Luis", "Ryan", "James", "Jack"],
+["Aiden", "Ian", "Colin" "Tim", "Cam"],
+["Larry", "Spencer", "Chris", "Ryan", "Nolan"]]
+
+
+for x in rows:
+    for name in x:
+        if(name.endswith("s")):
+            print("{0}\'".format(name))
+        else:
+            print("{0}\'s".format(name))
+
